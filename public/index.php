bb08c21c78d0360c154d536d46ee8f543cade7a7
Andraž Bajec <andraz.bajec27@gmail.com>
2020-10-03 22:44:59 +0200

    Added 404 not found page, updated router to also sort by request method

--- public/index.php
+++ public/index.php
@@ -1,16 +1,20 @@
 <?php
 $request = $_SERVER['REQUEST_URI'];
+$requestMethod = $_SERVER['REQUEST_METHOD'];
 
 require __DIR__ . '/views/header.html';
 
-switch ($request) {
-    case '/' :
-        require __DIR__ . '/views/home.php';
-        break;
-    default:
-        http_response_code(404);
-        require __DIR__ . '/views/404.php';
+switch ($requestMethod) {
+    case 'GET':
+        switch ($request) {
+            case '/' :
+                require __DIR__ . '/views/home.php';
+                break;
+            default:
+                http_response_code(404);
+                require __DIR__ . '/views/404.html';
+                break;
+        }
         break;
 }
-
 require __DIR__ . '/views/footer.html';
