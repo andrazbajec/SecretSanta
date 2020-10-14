e546207420db445689f3af14423a71215aceae85
Tamara Golobic <tamara.golobic@gmail.com>
2020-10-14 20:50:58 +0200

    Added home page, login page, updated routes, header and register

--- public/index.php
+++ public/index.php
@@ -8,7 +8,28 @@ switch ($requestMethod) {
     case 'GET':
         switch ($request) {
             case '/' :
+                require __DIR__ . '/views/home.phtml';
+                break;
+            case '/register' :
                 require __DIR__ . '/views/register.phtml';
+                break;
+            case '/login' :
+                require __DIR__ . '/views/login.phtml';
+                break;
+            case '/joinRoom':
+                require __DIR__ . '/views/joinRoom.phtml';
+                break;
+            case '/createRoom':
+                require __DIR__ . '/views/createRoom.phtml';
+                break;
+            case '/room':
+                require __DIR__ . '/views/room.phtml';
+                break;
+            case '/roomOptions':
+                require __DIR__ . '/views/room_options.phtml';
+                break;
+            case '/user':
+                require __DIR__ . '/views/user.phtml';
                 break;
             default:
                 http_response_code(404);
